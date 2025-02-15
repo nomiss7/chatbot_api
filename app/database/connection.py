1720e4ec94c920144681db9c3e57ed497c365d05
Nick <89870042+nomiss7@users.noreply.github.com>
2025-02-15 01:06:31 +0100

    db

--- app/database/connection.py
+++ app/database/connection.py
@@ -2,7 +2,8 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sess
 from sqlalchemy.orm import declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://chatbot:7@localhost/chatbot_db")
+# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://chatbot:7@localhost/chatbot_db")
+DATABASE_URL = "postgresql+asyncpg://chatbot:CpyZoSp796pSjuOqCBKNYhiG3T0mRQlA@dpg-cunti4dumphs73bqdgog-a.oregon-postgres.render.com/chatbot_db_spgr"
 
 async_engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 async_session = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
